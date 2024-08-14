c93dd7f0c3065c3b74157394873cb6eb9662050c
molxno <s.molano0818@gmail.com>
2024-08-14 11:50:40 -0500

    Fix database connection

--- src/resources/onPremiseResources/connectionClass.py
+++ src/resources/onPremiseResources/connectionClass.py
@@ -1,4 +1,3 @@
-# Implementa la conexión a la db
 import pyodbc
 import logging
 
@@ -7,46 +6,48 @@ from src.interfaces.resources.dbConnection import IDBConnection
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-
 class BancoRelationalDBResource(IDBConnection):
-
     def __init__(self):
-        server_address = 'PMEDMOLANOSA'
+        server_address = 'localhost'
         db_name = 'Banco'
-        username = 'santiago'
-        password = 'santiago'
-        self.connection = pyodbc.connect(
-            "DRIVER={SQL Server}; SERVER="+server_address+"; DATABASE ="+db_name+';UID='+username+';PWD=' + password)
+        connection_string = (
+            f"DRIVER={{ODBC Driver 17 for SQL Server}};"
+            f"SERVER={server_address};"
+            f"DATABASE={db_name};"
+            "Trusted_Connection=yes;"
+        )
+        try:
+            self.connection = pyodbc.connect(connection_string)
+            logger.info('Conexión a la base de datos exitosa')
+        except pyodbc.Error as e:
+            logger.error(f'Error de conexión: {e}')
+            raise
 
     def db_connection(self, query, sp_type):
-        # Metodo de conexion y consulta a Banco
         logger.info('Inicio conexión a Banco')
-
-        with self.connection as connection:
-            cursor = connection.cursor()
-            # OK! conexión exitosa
-            logger.info('Conexion exitosa con Banco')
-            # Inicio de consulta
-            logger.info(f'Inicio ejecución de la query de {sp_type}')
-            cursor.execute(query)
-            # consulta exitosa
-            logger.info('Ejecucion de la query Exitosa')
-            sp_type_list = [dict(line) for line in [zip(
-                [column[0] for column in cursor.description], row) for row in cursor.fetchall()]]
-            return sp_type_list
+        try:
+            with self.connection.cursor() as cursor:
+                logger.info('Conexión exitosa con Banco')
+                logger.info(f'Inicio ejecución de la query de {sp_type}')
+                cursor.execute(query)
+                logger.info('Ejecución de la query exitosa')
+                sp_type_list = [dict(line) for line in [zip(
+                    [column[0] for column in cursor.description], row) for row in cursor.fetchall()]]
+                return sp_type_list
+        except pyodbc.Error as e:
+            logger.error(f'Error durante la consulta: {e}')
+            raise
 
     def db_connection_result(self, query, sp_type):
-        # Metodo de conexion y consulta a Banco
         logger.info('Inicio conexión a Banco')
-
-        with self.connection as connection:
-            cursor = connection.cursor()
-            # OK! conexión exitosa
-            logger.info('Conexion exitosa con Banco')
-            # Inicio de consulta
-            logger.info(f'Inicio ejecución de la query de {sp_type}')
-            cursor.execute(query)
-            # consulta exitosa
-            logger.info('Ejecucion de la query Exitosa')
-            message = {"Message": "Se ha ejecutado de manera correcta el query"}
-            return message
+        try:
+            with self.connection.cursor() as cursor:
+                logger.info('Conexión exitosa con Banco')
+                logger.info(f'Inicio ejecución de la query de {sp_type}')
+                cursor.execute(query)
+                logger.info('Ejecución de la query exitosa')
+                message = {"Message": "Se ha ejecutado de manera correcta el query"}
+                return message
+        except pyodbc.Error as e:
+            logger.error(f'Error durante la consulta: {e}')
+            raise
